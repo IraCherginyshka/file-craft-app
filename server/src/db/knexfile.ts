4bc72c88d7ff2f3b54d7957a2408b75829882f34
Iryna Cherhina <icherhina@gmail.com>
2025-06-19 13:08:29 +0200

    migrate to ts

---
+++ server/src/db/knexfile.ts
@@ -0,0 +1,54 @@
+import dotenv from 'dotenv';
+import { Knex } from 'knex';
+
+// todo prod
+dotenv.config({ path: `../.env.${process.env.NODE_ENV}` });
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ *
+ *   host: process.env.DB_HOST,
+ *   port: process.env.DB_PORT,
+ *   user: process.env.DB_USER,
+ *   password: process.env.DB_PASSWORD,
+ *   database: process.env.DB_NAME,
+ */
+const config: Knex.Config = {
+  client: 'pg',
+  connection: {
+    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+    port: Number(process.env.DB_PORT) || 5432,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    directory: './migrations',
+    tableName: 'knex_migrations',
+  },
+
+  // todo
+  // production: {
+  //   client: 'postgresql',
+  //   connection: {
+  //     database: 'my_db',
+  //     user:     'username',
+  //     password: 'password'
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     tableName: 'knex_migrations'
+  //   }
+  // }
+};
+
+console.log(config);
+
+export default config;
